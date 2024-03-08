b7f93d9181b8ff63ae624ba0f4040c47b56d87a8
Johannes Doll <git@johannesdoll.com>
2024-03-08 20:10:15 +0100

    Initial

---
+++ include/EspKeyboard.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <Keypad.h>      // Keypad library to handle matrix keypad setup
+#include <BleKeyboard.h> // For ESP32 Bluetooth keyboard HID https://github.com/T-vK/ESP32-BLE-Keyboard
+
+void setup();
+void loop();
---
+++ src/EspKeybaord.cpp
@@ -0,0 +1,1 @@
+#include "EspKeyboard.h"
